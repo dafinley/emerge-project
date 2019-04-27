10510ac607a274e22d868846c8c9f7e1d59b190b
Douglas Finley <douglasfinley@Douglass-MacBook-Pro.local>
2019-04-27 11:52:28 -0400

    some foundation

---
+++ frontend/src/routes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Layout Types
+import { DefaultLayout } from "./layouts";
+
+// Route Views
+import BlogOverview from "./views/BlogOverview";
+import UserProfileLite from "./views/UserProfileLite";
+//import AddNewPost from "./views/AddNewPost";
+//import Errors from "./views/Errors";
+//import ComponentsOverview from "./views/ComponentsOverview";
+//import Tables from "./views/Tables";
+//import BlogPosts from "./views/BlogPosts";
+
+export default [
+  {
+    path: "/",
+    exact: true,
+    layout: DefaultLayout,
+    component: () => <Redirect to="/blog-overview" />
+  },
+  {
+    path: "/blog-overview",
+    layout: DefaultLayout,
+    component: BlogOverview
+  },
+  {
+    path: "/user-profile-lite",
+    layout: DefaultLayout,
+    component: UserProfileLite
+  },
+];
---
+++ frontend/src/views/BlogOverview.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col } from "shards-react";
+
+import PageTitle from "./../components/common/PageTitle";
+
+const BlogOverview = ({ smallStats }) => (
+  <Container fluid className="main-content-container px-4">
+    {/* Page Header */}
+    <Row noGutters className="page-header py-4">
+      <PageTitle title="Blog Overview" subtitle="Dashboard" className="text-sm-left mb-3" />
+    </Row>
+
+    {/* Small Stats Blocks */}
+    <Row>
+      
+    </Row>
+
+    <Row>
+      
+    </Row>
+  </Container>
+);
+
+BlogOverview.propTypes = {
+  /**
+   * The small stats dataset.
+   */
+  smallStats: PropTypes.array
+};
+
+BlogOverview.defaultProps = {
+  smallStats: []
+};
+
+export default BlogOverview;
